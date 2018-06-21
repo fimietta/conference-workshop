2522d1e05224b4df4cd77b9768f02a0b036323c1
Daniela Remogna <daniela.remogna@crowdstrike.com>
2018-06-21 14:59:35 +0100

    Generate agenda route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('home');
+  this.route('agenda');
 });
 
 export default Router;
---
+++ tests/unit/routes/agenda-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:agenda', 'Unit | Route | agenda', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
