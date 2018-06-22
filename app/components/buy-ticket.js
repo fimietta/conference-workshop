60a406b70e32645eda0d53a0889c46b68dca36a4
Daniela Remogna <daniela.remogna@crowdstrike.com>
2018-06-22 13:42:29 +0100

    Add a simple action to buy a ticket

--- app/components/buy-ticket.js
+++ app/components/buy-ticket.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  name: '',
+
+  actions: {
+    buy(name) {
+      alert(`${name} bought a ticket`);
+    }
+  }
 });
