723bd8663090dade97e86e8cb875f434ef9a91a2
Daniela Remogna <daniela.remogna@crowdstrike.com>
2018-06-22 13:37:15 +0100

    Add speakers template view and model

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    // Usually there is an AJAX call here
+    return [
+      {
+        id: 1,
+        name: 'Daniela Remogna',
+        description: 'Daniela Remogna speaker description',
+      },
+      {
+        id: 2,
+        name: 'John Smith',
+        description: 'John Smith speaker description',
+      },
+      {
+        id: 3,
+        name: 'Megan Ford',
+        description: 'Megan ford speaker description',
+      },
+    ];
+  },
 });
